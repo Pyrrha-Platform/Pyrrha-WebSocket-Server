bd821f67f2453684478af519c4edefa7a9fc2699
Upkar Lidder <upkar@pm.me>
2021-03-03 10:37:44 -0800

    adding a ping test to remove dead clients

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const logConfiguration = {
   ]
 };
 
+function noop() { }
+
+function heartbeat() {
+  this.isAlive = true;
+}
+
 const logger = new winston.createLogger(logConfiguration)
 const PORT = process.env.PORT || 8090;
 
@@ -14,26 +20,37 @@ const wss = new WebSocket.Server({ port: PORT });
 
 logger.info(`Started websocket server on port: ${PORT}`)
 
-// Broadcast to all.
-// wss.broadcast = function broadcast(data) {
-//   wss.clients.forEach(function each(client) {
-//     if (client.readyState === WebSocket.OPEN) {
-//       client.send(data);
-//     }
-//   });
-// };
+wss.on('connection', function connection(ws, req) {
 
-wss.on('connection', function connection(ws) {
+  const ip = req.socket.remoteAddress;
+  logger.info(`new client connected: ${ip}`);
+
+  // set up alive check to terminate if client does not pong back
+  ws.isAlive = true;
+  ws.on('pong', heartbeat);
+
   ws.on('message', function incoming(data) {
     logger.info(`logger, incoming data: ${data}`);
-    logger.info(`logger, Broadcasting message to ${wss.clients.size - 1} clients`);
-    console.log(`incoming data: ${data}`);
-    console.log(`Broadcasting message to ${wss.clients.size - 1} clients`);
+
     // Broadcast to everyone else.
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
+        logger.info(`logger, Broadcasting message to ${wss.clients.size - 1} clients`);
         client.send(data);
       }
     });
   });
+});
+
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping(noop);
+  });
+}, 30000);
+
+wss.on('close', function close() {
+  clearInterval(interval);
 });
